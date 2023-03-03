508381985aebb2c3ba3b064e74348c56a9b87f4f
Gustavo Ariel Retamar <121575510+gustavoarielretamar@users.noreply.github.com>
2023-03-03 10:21:59 -0300

    Update Incendio_forestal.py

--- Incendio_forestal.py
+++ Incendio_forestal.py
@@ -13,8 +13,8 @@ def generar_bosque (n, debug = False):
 #%% SUCESO ALEATORIO
 def suceso_aleatorio (p, debug = False):
     suceso = False
-    prob = random.randint(0, 100)
-    if prob <= p:
+    num_aleatorio = random.randint(0, 100)
+    if num_aleatorio <= p:
         suceso = True
     if debug:
         print(suceso)
@@ -55,17 +55,30 @@ def rayos(bosque, f, debug = False):
 # rayos (brotes(generar_bosque(10), 80, True), 30, True)
 
 #%% 05 PROPAGACION
-def propagacion (bosque, debug = False):
-    for i in range (len(bosque)):
-        if bosque [i] == -1:
-            if bosque [i + 1] == 1:
-                bosque [i + 1] = -1
-            if bosque [i - 1] == 1:
-                bosque [i - 1] = -1
-    if debug:
-        print (bosque)
-    return bosque
+#def propagacion (bosque, debug = False):
+#    for i in range (len(bosque)):
+#        if bosque [i] == -1:
+#            if bosque [i + 1] == 1:
+#                bosque [i + 1] = -1
+#            if bosque [i - 1] == 1:
+#                bosque [i -1] == -1
+#    if debug:
+#        print (bosque)
+#    return bosque
 # propagacion(rayos (brotes(generar_bosque(10), 80), 30), True)
+
+def propagacion (bosque):
+    i = 0
+    while i < (len(bosque) - 1):
+        if bosque[i] == - 1 and bosque [i + 1] == 1:
+            bosque [i + 1] = - 1
+        i = i + 1
+    i = len (bosque) - 1
+    while i > 0:
+        if bosque [i] == - 1 and bosque [i - 1] == 1:
+            bosque [i - 1] = -1
+        i = i + 1
+    return bosque
 
 #%% 06 LIMPIEZA
 def limpieza (bosque, debug = False):
@@ -79,7 +92,7 @@ def limpieza (bosque, debug = False):
 
 #%% 07 PROGRAMA
 def dinamica (n, a, p, f, debug = False):
-    arboles = [] * a
+    arboles = []
     contador = 0
     while contador < a:
         bosque = generar_bosque (n)
@@ -95,21 +108,20 @@ def dinamica (n, a, p, f, debug = False):
     return promedio
 
 def promedios (n, a, p, f):
-    promedios = []* a
+    promedios = []
     contador = 0
     while contador <= 10:
         promedios.append(dinamica (n, a, p, f))
         contador = contador + 1
         p = p + 10
     print(promedios)
-    return promedios
-lista_promedios = promedios(100, 1000, 0, 2)
+    return promedios,
+lista_promedios = promedios(10, 10, 0, 2)
+lista_p = range (0, 100, 1)
 
-plt.title("titulo del grafico")
-plt.xlabel("valores de x", fontsize = 16)
-plt.ylabel("valores de y", color = "blue")
-plt.plot(lista_promedios, ".")
-plt.show()
+#plt.title("titulo del grafico")
+#plt.plot(lista_p, lista_promedios, marker='.')
+#plt.show()
 
 
 # ax.lista_promedios
